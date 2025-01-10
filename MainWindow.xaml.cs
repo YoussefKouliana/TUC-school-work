b27c75e81ad41ebc3d67a88a9893ab40f6aee79f
YoussefKouliana <yosep.koliana@gmail.com>
2025-01-10 11:20:10 +0100

    Update logic

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace WpfApp1
 {
@@ -16,14 +7,41 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private TodoList _todoList;
         public MainWindow()
         {
             InitializeComponent();
+            _todoList = new TodoList();
         }
 
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
+            string task = TaskTextBox.Text;
+            if (!string.IsNullOrEmpty(task))
+            {
+                _todoList.AddTask(task);
+                UpdateTaskList();
+                TaskTextBox.Clear();
+            }
+        }
 
+        private void UpdateTaskList()
+        {
+            TasksListBox.Items.Clear();
+            foreach (var task in _todoList.GetAllTasks())
+            {
+                TasksListBox.Items.Add(task);
+            }
+        }
+
+        private void RemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (TasksListBox.SelectedIndex >= 0)
+            {
+                _todoList.RemoveTask(TasksListBox.SelectedIndex);
+                UpdateTaskList();
+            }
         }
     }
+
 }
