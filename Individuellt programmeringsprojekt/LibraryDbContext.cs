5f94d4eb30bd35031e1421d1c9a98f3f30c39ca7
YoussefKouliana <yosep.koliana@gmail.com>
2025-01-14 00:30:34 +0100

    Add project files.

---
+++ Individuellt programmeringsprojekt/LibraryDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+
+namespace Individuellt_programmeringsprojekt
+{
+    public class LibraryDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+
+
+        public LibraryDbContext() : base("LibraryConnectionString")
+        {
+        }
+    }
+}
